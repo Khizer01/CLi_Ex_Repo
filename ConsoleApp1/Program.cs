ff2f0bd81707e22fc8b72168d9733eaf42b25f7f
MishalKhalid02 <mishalkhalid445@gmail.com>
2025-12-02 23:31:03 +0500

    Upadted API key

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string apiKey = "AIzaSyAeulJakYAFxeA2PGRg9a5RH2KA03C95ak";
+            string apiKey = "AIzaSyDNX1my7VNyONDLV9TS3vN6pGIXaQFsfhA";
             CommandShell shell = new CommandShell(apiKey);
             shell.Run();
         }
